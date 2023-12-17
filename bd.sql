cbd5c7d06d41d404a7b4370da211a1e2d6b85b2a
Guilherme Rabelo <guilhermerabelo699@gmail.com>
2023-12-17 09:23:50 -0300

    Feat/Create do banco de dados

---
+++ bd.sql
@@ -0,0 +1,53 @@
+create table usuarios (
+	id SERIAL NOT NULL,
+	nome VARCHAR(150) NOT NULL,
+	email VARCHAR(100) NOT NULL UNIQUE,
+	senha_hash VARCHAR(100) NOT NULL,
+	data_create TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+create table checklistMensal (
+	id SERIAL,
+	item VARCHAR(50) not null,
+	dia_mes INTEGER not null,
+	user_id INTEGER not null,
+	data_create TIMESTAMP DEFAULT current_timestamp not null,
+	
+	primary key(id)
+)
+
+ALTER TABLE checklistMensal ADD CONSTRAINT FK_checklistMensal_usuarios
+FOREIGN KEY (user_id) REFERENCES usuarios(id)
+
+create table tipoMovimento (
+	id SERIAL,
+	item VARCHAR(15) not null,
+	data_create TIMESTAMP DEFAULT current_timestamp not null,
+	
+	primary key(id)
+)
+
+create table movimento (
+	id SERIAL,
+	descricao VARCHAR(100) not null,
+	data TIMESTAMP not null,
+	valor NUMERIC(9,2) NOT null,
+	tipoMovimento_id INTEGER not null,
+	user_id INTEGER not null,
+	checklistMensal_id INTEGER not null,
+	data_create TIMESTAMP DEFAULT current_timestamp not null,
+	
+	primary key(id)
+)
+
+
+ALTER TABLE movimento ADD CONSTRAINT FK_movimento_usuarios
+FOREIGN KEY (user_id) REFERENCES usuarios(id)
+
+ALTER TABLE movimento ADD CONSTRAINT FK_movimento_tipoMovimento
+FOREIGN KEY (tipoMovimento_id) REFERENCES tipoMovimento(id)
+
+ALTER TABLE movimento ADD CONSTRAINT FK_movimento_checklistMensal
+FOREIGN KEY (checklistMensal_id) REFERENCES checklistMensal(id)
+
